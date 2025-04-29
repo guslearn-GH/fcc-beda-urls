95e911c3e4433b15e1b7d443ffd550decb714595
Gus <56235860+guslearn-GH@users.noreply.github.com>
2025-04-29 11:45:23 +0000

    #

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  dns.lookup(req.body.url, (err, addr, fam) => {
+    err != null
+      ? res.json({ status: "invalid" })
+      : res.json({ original_url: req.body.url, short_url: 1 });
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
