464c77cd94636c2ce2480a67d5c1cbcd42946b3d
Gus <56235860+guslearn-GH@users.noreply.github.com>
2025-04-29 18:20:14 +0000

    #

--- index.js
+++ index.js
@@ -23,61 +23,6 @@ let ShortUrlSchema = mongoose.Schema({
 
 let ShortUrl = mongoose.model("ShortUrl", ShortUrlSchema);
 
-CreateShortURL = (original_url) => {
-  // mongoose.connect(process.env.MONGO_URI, {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // });
-  // let dbConn = mongoose.connection;
-  // //dbConn.on("open", () => console.log("Database open"));
-  // //dbConn.on("close", () => Console.log("Database closed"));
-  let res = 999;
-  // dbConn.once("open", () => {
-  //   console.log("connected");
-  // });
-  // //{
-  // //dbConn.on("open", () => console.log("connected"));
-  // //dbConn.useDb("test");
-  // //console.log("connected");
-  // // console.log(dbConn.db.databaseName);
-  // // console.log(dbConn.collections);
-  // // ShortUrl.createCollection().then(
-  // //   console.log("Collection is created? check!!")
-  // // );
-  // // console.log(dbConn.collections);
-  // // console.log("new Collection? " + dbConn.collection("short_urls"));
-
-  // //console.log(dbConn.collection("shorturls"));
-  // //.countDocuments().exec());
-  // //res = dbConn.collection("shorturls").countDocuments();
-  var count = ShortUrl.countDocuments({}, (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("# of Docs:" + data);
-      count = data;
-    }
-  });
-  ShortUrl.create({
-    urlID: count,
-    fullURL: original_url,
-  });
-  // //});
-  // dbConn.on("close", () => Console.log("Database closed"));
-  // dbConn.close();
-
-  return "/api/shorturl/" + count;
-};
-
-RetrieveOrignalURL = (shortUrlId) => {
-  dbConn.open();
-  dbConn.useDb();
-  let res = ShortUrl.find({ urlID: shortUrlId }).urlID;
-  dbConn.close();
-  return res;
-};
-//mongoose.connection.close();
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -100,18 +45,50 @@ app.post("/api/shorturl", function (req, res) {
   dns.lookup(req.body.url, (err, addr, fam) => {
     err != null
       ? res.json({ status: "invalid" })
-      : res.json({
-          original_url: req.body.url,
-          short_url: CreateShortURL(req.body.url),
-        });
+      : CreateShortURL(req.body.url, res);
     //store url after validating
   });
 });
 
+CreateShortURL = async (full_url, resp) => {
+  let count = await ShortUrl.countDocuments({});
+  console.log("# of Docs: " + count);
+  ShortUrl.create({ urlID: count, fullURL: full_url }, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(
+        "original URL: " + full_url + " <> short URL: /api/shorturl/" + count
+      );
+      return resp.json({
+        original_url: full_url,
+        short_url: "/api/shorturl/" + count,
+      });
+    }
+  });
+};
+
+RetrieveOrignalURL = (shortUrlId) => {
+  ShortUrl.find({ urlID: shortUrlId }).exec((err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Go to: " + data[0].fullURL);
+
+      window.open(data[0].fullURL);
+    }
+  });
+};
+
 app.get("/api/shorturl/:uriID", function (req, res) {
-  replace(RetrieveOrignalURL(req.params.uriID));
+  //window.location.replace(
+  RetrieveOrignalURL(req.params.uriID);
+  //);
 });
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+exports.CreateShortURL = this.CreateShortURL;
+exports.RetrieveOrignalURL = RetrieveOrignalURL;
